d084850a8390071fe9dd63e12b34b30ec7ba10eb
nathanjh-28 <nathan@nathanjharris.com>
2025-08-13 11:01:57 -0400

    fixing vercel config 3

--- Video-Project-Suite.Client/src/services/api.jsx
+++ Video-Project-Suite.Client/src/services/api.jsx
@@ -1,5 +1,7 @@
 // src/services/api.js
-const API_BASE_URL = '/api'; // Replace with your API URL
+// const API_BASE_URL = '/api'; // Replace with your API URL
+const API_BASE_URL = import.meta.env.VITE_API_URL || '/api';
+
 
 // Mock data for development
 const mockProjects = [
